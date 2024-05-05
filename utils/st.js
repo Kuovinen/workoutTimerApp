2b49fd3583978c9f8df6f4f1a581affa1eae168d
Emil <emil.kuovinen@gmail.com>
2024-05-06 02:11:08 +0300

    Add basic styling values

---
+++ utils/st.js
@@ -0,0 +1,17 @@
+const st = {
+  light: "#A6DEF0",
+  bkg: "#070A0B",
+  wht: "#EEF3F5",
+  dark: "#3694B4",
+  tst2: "#369414",
+  tst3: "#b45636",
+  btn: "rgba(21, 58, 71, 0.5)" /*#3694b4*/,
+  trns: "rgba(21, 58, 71, 0.0)" /*#3694b4*/,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+  xxxl: 80,
+};
+export default st;
