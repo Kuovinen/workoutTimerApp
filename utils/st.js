8538d556a003f2e35737ff5aae7de364c71b3fa5
Emil <emil.kuovinen@gmail.com>
2024-05-06 18:01:08 +0300

    Add yellow values

--- utils/st.js
+++ utils/st.js
@@ -1,11 +1,13 @@
 const st = {
   light: "#A6DEF0",
+  lightY: "#EAC98A",
   bkg: "#070A0B",
   wht: "#EEF3F5",
   dark: "#3694B4",
   tst2: "#369414",
   tst3: "#b45636",
   btn: "rgba(21, 58, 71, 0.5)" /*#3694b4*/,
+  btnY: "rgba(92, 79, 54, 0.5)" /*#3694b4*/,
   trns: "rgba(21, 58, 71, 0.0)" /*#3694b4*/,
   xs: 4,
   sm: 8,
